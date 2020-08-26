905851b8f7c36ce11af618e9dae2314a077e15bb
Ismaila Abdoulahi <code.ismaila@gmail.com>
2020-08-26 16:10:08 +0200

    Add eslint & prettier config

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,25 @@
-import svelte from "rollup-plugin-svelte";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import livereload from "rollup-plugin-livereload";
-import pkg from "./package.json";
+import svelte from 'rollup-plugin-svelte';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import livereload from 'rollup-plugin-livereload';
+import pkg from './package.json';
 
+// eslint-disable-next-line no-undef
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-  input: "src/index.js",
+  input: 'src/index.js',
   output: [
-    { file: pkg.module, format: "es" },
-    { file: pkg.main, format: "umd", name: pkg.name },
+    { file: pkg.module, format: 'es' },
+    { file: pkg.main, format: 'umd', name: pkg.name }
   ],
   plugins: [
     svelte({
-      include: "src/**/*.svelte",
-      dev: true,
+      include: 'src/**/*.svelte',
+      dev: true
     }),
     resolve(),
     commonjs(),
-    !production && livereload("public"),
-  ],
+    !production && livereload('public')
+  ]
 };
