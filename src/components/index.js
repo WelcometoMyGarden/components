59962f2951f1e417fc86e9d01473e9af72eb3da1
Ismaila Abdoulahi <code.ismaila@gmail.com>
2020-08-27 15:13:22 +0200

    Add TopNav

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Button } from './Button.svelte';
 export { default as Text } from './Text.svelte';
 export { default as NavLink } from './Text.svelte';
+export { default as TopNav } from './Text.svelte';
