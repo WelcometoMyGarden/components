6445b2497699f6b0474e644341432da95d92f37b
Ismaila Abdoulahi <code.ismaila@gmail.com>
2020-08-27 18:22:18 +0200

    Add Brand component& slotify TopNav

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Button } from './Button.svelte';
 export { default as Text } from './Text.svelte';
-export { default as NavLink } from './Text.svelte';
-export { default as TopNav } from './Text.svelte';
+export { default as NavLink } from './NavLink.svelte';
+export { default as TopNav } from './TopNav.svelte';
+export { default as Brand } from './Brand.svelte';
---
+++ src/views/TopNav.svelte
@@ -0,0 +1,58 @@
+<script>
+  import { TopNav, NavLink, Brand } from '../components/';
+  let selected = '/';
+
+  const handleClick = (e, link) => {
+    e.preventDefault();
+    selected = link;
+  };
+</script>
+
+<TopNav>
+  <span slot="brand">
+    <Brand
+      name="Welcome To My Garden"
+      logo="/logo.svg"
+      href="/"
+      on:click={(e) => handleClick(e, '/')} />
+  </span>
+  <ul slot="links">
+    <li>
+      <NavLink href="/" active={selected === '/'} on:click={(e) => handleClick(e, '/')}>
+        Home
+      </NavLink>
+    </li>
+    <li>
+      <NavLink href="/map" active={selected === '/map'} on:click={(e) => handleClick(e, '/map')}>
+        Map
+      </NavLink>
+    </li>
+    <li>
+      <NavLink
+        href="/rules"
+        active={selected === '/rules'}
+        on:click={(e) => handleClick(e, '/rules')}>
+        Rules
+      </NavLink>
+    </li>
+    <li>
+      <NavLink href="/faq" active={selected === '/faq'} on:click={(e) => handleClick(e, '/faq')}>
+        FAQ
+      </NavLink>
+    </li>
+    <li>
+      <NavLink
+        href="/sign-in"
+        active={selected === '/sign-in'}
+        on:click={(e) => handleClick(e, '/sign-in')}>
+        Sign in
+      </NavLink>
+    </li>
+  </ul>
+</TopNav>
+
+<style>
+  span {
+    height: 100%;
+  }
+</style>
